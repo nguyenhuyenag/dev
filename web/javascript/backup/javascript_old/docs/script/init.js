426fdf30d1ae0291410b0439bcbcf8c0ea286396
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-06 20:57:13 +0700

    #

--- web/javascript/backup/javascript_old/docs/script/init.js
+++
@@ -1,7 +0,0 @@
-/**
-* - JavaScript phân biệt hoa thường, ví dụ `myVariable` và `myvariable` là khác nhau
-*/
-
-document.querySelector('html').onclick = function () {
-    alert('Ouch! Stop poking me!');
-}

174cb73446142afa2d3b56c2d3116b22287d863d
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2026-03-16 07:53:34 +0700

    Auto commit

--- git/auto-pull-commit.bat
+++ git/auto-pull-commit.bat
@@ -1,5 +1,4 @@
 @echo off
-chcp 65001 >nul
 
 :: Current folder
 set "GIT_FOLDER=%cd%"
@@ -18,16 +17,7 @@ for /f %%f in ('dir /ad /b "%GIT_FOLDER%"') do (
             git push
         ) else (
             :: Pull và kiểm tra kết quả
-            git pull > temp_pull.txt
-
-            findstr /C:"Already up to date." temp_pull.txt >nul
-            if not errorlevel 1 (
-                echo [OK] Already up to date.
-            ) else (
-                type temp_pull.txt
-            )
-
-            del temp_pull.txt
+            git pull
         )
 
         echo.
